e2a94ca24f00d578e144071d4c3bf5642ced603e
andypagdin <andyjpagdin@gmail.com>
2020-03-31 23:24:45 +0100

    disable prop-type checking

--- src/components/board.js
+++ src/components/board.js
@@ -4,7 +4,7 @@ import Cell from './Cell'
 const Board = ({ problem }) => {
   const cells = []
   for (let i = 0; i < problem.length; i++) {
-    let row = problem[i]
+    const row = problem[i]
     for (let x = 0; x < row.length; x++) {
       cells.push(<Cell value={row[x]} />)
     }
